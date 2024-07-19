3a707a67441cffe15cd9b2024e200934e81675f9
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2024-07-19 19:44:11 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+// Para não utilizar esse arquivo que é um preset de configuração basta passar ele pelo comando
+// do terminal ou pelo script do package.json 
+module.exports = {
+  presets: [
+    ["@babel/preset-env"]
+  ]
+}
